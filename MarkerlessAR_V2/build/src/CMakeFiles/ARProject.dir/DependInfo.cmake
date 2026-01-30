eea94c03938ddf693558f94d23f11a2699672d27
root <shreeya607@gmail.com>
2026-01-29 19:46:37 -0600

    this version of cmake builds on fpga

---
+++ MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/ARDrawingContext.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/ARDrawingContext.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/ARPipeline.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/ARPipeline.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/CameraCalibration.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/CameraCalibration.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/GeometryTypes.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/GeometryTypes.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/Pattern.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/Pattern.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/PatternDetector.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/PatternDetector.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/main.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/main.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/objloader.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/objloader.cpp.o"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/src/texture.cpp" "/home/unc-design/augmented-reality-glasses/AR_Application_Software/MarkerlessAR_V2/build/src/CMakeFiles/ARProject.dir/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/build"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/include/opencv"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/core/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/flann/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/imgproc/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/ml/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/photo/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/video/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/dnn/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/features2d/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/imgcodecs/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/shape/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/videoio/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/calib3d/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/highgui/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/objdetect/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/stitching/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/superres/include"
+  "/home/unc-design/augmented-reality-glasses/AR_Application_Software/opencv/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
