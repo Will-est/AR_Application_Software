45f3ae8ba187935c3e9564972e07ffe40ca664c4
Will-est <rwillpainter@gmail.com>
2026-02-02 10:14:39 -0600

    mks it possible to see  homorphy

--- MarkerlessAR_V2/src/ARDrawingContext.cpp
+++ MarkerlessAR_V2/src/ARDrawingContext.cpp
@@ -16,6 +16,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <unistd.h>
+#include <limits.h>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -73,14 +75,15 @@ ARDrawingContext::ARDrawingContext(std::string windowName, cv::Size frameSize, c
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_FRONT);
 
+	
 	// Load .bmp file as texture
 	Texture=loadBMP_custom("../Artifacts/green_cube.bmp");
-	
+
     // Load .dds file as texture (uvmap)
     //Texture=loadDDS("../Artifacts/uvmap.dds");
 
     // load(parse) .obj file
-	res = loadOBJ("../Artifacts/green_cube.obj", vertices, uvs, normals);
+	res = loadOBJ("../Artifacts/testcube.obj", vertices, uvs, normals);
 
 	// Scale 3D Model
     scale3DModel(0.1f);
