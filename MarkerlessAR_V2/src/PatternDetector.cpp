c556be7ad6e116105c386402bff14d272028063b
Shreeya Garapaty <shreeya607@gmail.com>
2026-04-10 14:57:28 -0500

    Fix grayscale conversion bug in pattern detection

--- MarkerlessAR_V2/src/PatternDetector.cpp
+++ MarkerlessAR_V2/src/PatternDetector.cpp
@@ -91,8 +91,8 @@ bool PatternDetector::findPattern(const cv::Mat& image, PatternTrackingInfo& inf
 {
     // Convert input image to gray
     getGray(image, m_grayImg);
-    // gaussian the image
-    cv::GaussianBlur(image, m_grayImg, cv::Size(5,5), 0);
+    // gaussian the image (blur the grayscale image, not the color input)
+    cv::GaussianBlur(m_grayImg, m_grayImg, cv::Size(5,5), 0);
 
     extractFeatures(m_grayImg, m_queryKeypoints, m_queryDescriptors);
     
