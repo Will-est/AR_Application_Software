6199232ae7ac36643eea9a9d738b37bb33692c43
Shreeya Garapaty <shreeya607@gmail.com>
2026-04-12 17:02:39 -0500

    Fix COLLECTDA linkage mismatch

--- MarkerlessAR_V2/src/PatternDetector.cpp
+++ MarkerlessAR_V2/src/PatternDetector.cpp
@@ -8,7 +8,6 @@
 --- Demo Video     : https://www.youtube.com/watch?v=nPfR5ACrqu0
 ---------------------------------------------------------------------
 */
-#define COLLECTDA 1
 // File includes:
 #include "PatternDetector.hpp"
 #include "CollectDa.hpp"
